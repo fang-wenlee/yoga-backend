8fd0e2304720e4ba448af11f88171d23047d1aef
fang-wenlee <fangwenlee@hotmail.com>
2026-01-01 11:31:15 -0800

    adding app.get to test rote to confirm setting

--- server.js
+++ server.js
@@ -13,5 +13,7 @@ app.use(express.json());
 // Upload route
 app.use("/upload", uploadRoute);
 
+app.get("/", (req, res) => res.send("Server is running"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
